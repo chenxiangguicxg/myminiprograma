d761d0eec20f89820e9442b8f3b2802598a5a4a7
chenxiangguicxg <1140581941@qq.com>
2020-05-22 14:12:34 +0800

    增加长按删除条目抖动功能

--- components/x-imageItem/x-imageItem.js
+++ components/x-imageItem/x-imageItem.js
@@ -11,13 +11,38 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    isDelItem: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    _delImageItem: function() {
+      this.setData({
+        isDelItem: true
+      })
+    },
+    _handlerDelete: function () {
+      wx.showModal({
+        title: '删除本条目',
+        content: '是否确认删除本条目',
+        success: (res) => {
+          if (res.confirm) {
+            // 这里并没有删除数据，后面处理
+            wx.showToast({
+              title: '删除成功',
+            })
+            this.setData({
+              isDelItem: false
+            })
+          } else if (res.cancel) {
+            this.setData({
+              isDelItem: false
+            })
+          }
+        }
+      })
+    }
   }
 })
